6168e4c8f8435a970e0d001bfe4e8519c2769184
shaack <shaack@gmail.com>
2024-02-05 11:08:22 +0100

    Index displayed and dark theme for the modal

--- src/LightboxGallery.js
+++ src/LightboxGallery.js
@@ -8,15 +8,18 @@ import "bootstrap-show-modal/src/ShowModal.js"
 
 export class LightboxGallery {
 
-    constructor(elements, title, props = {}) {
+    constructor(elements, props = {}) {
         this.props = {
-            id: "lightboxGallery"
+            id: "lightboxGallery",
+            props: undefined
         }
         Object.assign(this.props, props)
         // this.elements = elements
         this.state = {
-            title: title,
-            carousel: undefined
+            title: props.title,
+            carousel: undefined,
+            itemCount: 0,
+            currentIndex: undefined
         }
         this.init(elements)
     }
@@ -36,24 +39,32 @@ export class LightboxGallery {
                 const targetLink = event.target.closest("a")
                 this.open(targetLink)
             })
+            let caption = ""
+            if (itemData.caption) {
+                caption = `<div class="rounded carousel-caption d-none d-md-block p-2 text-light bg-dark bg-opacity-50">
+                             ${itemData.caption}
+                           </div>`
+            }
             const carouselItem = `
-                <div class="carousel-item h-100">
+                <div class="carousel-item h-100" data-bs-index="${this.state.itemCount}">
                     <div class="d-flex align-items-center h-100 w-100">
-                        <img src="${itemData.url}" class="d-block mx-auto" title="${itemData.title}" alt="${itemData.alt}"/>
+                        <img src="${itemData.url}" class="d-block mx-auto img-fluid" title="${itemData.title}" alt="${itemData.alt}"/>
+                        ${caption}
                     </div>
                 </div>`
             carouselItems += carouselItem
+            this.state.itemCount++
         })
         this.state.carouselElement = DomUtils.createElement(`
 <div id="${this.props.id}" class="carousel slide h-100">
   <div class="carousel-inner h-100">
     ${carouselItems}
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#${this.props.id}" data-bs-slide="prev">
+  <button style="filter: invert(1) grayscale(100);" class="carousel-control-prev" type="button" data-bs-target="#${this.props.id}" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Vorheriges Bild</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#${this.props.id}" data-bs-slide="next">
+  <button style="filter: invert(1) grayscale(100);"  class="carousel-control-next" type="button" data-bs-target="#${this.props.id}" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Nächstes Bild</span>
   </button>
@@ -67,15 +78,30 @@ export class LightboxGallery {
             carouselItem.classList.remove("active")
         })
         const image = this.state.carouselElement.querySelector(`[src="${targetLink.href}"]`)
-        image.closest(".carousel-item").classList.add("active")
+        const activeItem = image.closest(".carousel-item")
+        activeItem.classList.add("active")
+        this.state.currentIndex = parseInt(activeItem.getAttribute("data-bs-index"))
         if (targetLink) {
-            bootstrap.showModal({
-                title: this.state.title,
+            this.modal = bootstrap.showModal({
+                theme: "dark",
+                title: this.state.title + " <small class='text-muted ms-3 pb-1 carousel-index'></small>",
                 body: this.state.carouselElement.outerHTML,
-                modalClass: "",
                 modalDialogClass: "modal-fullscreen"
             })
+            const carouselElement = this.modal.element.querySelector(".carousel")
+            carouselElement.addEventListener('slide.bs.carousel', event => {
+                console.log("slide.bs.carousel", event)
+                this.state.currentIndex = event.to
+                this.updateIndex()
+            })
         }
+        this.updateIndex()
+    }
+
+    updateIndex() {
+        // const activeItem = this.state.carouselElement.querySelector(".carousel-item.active")
+        // this.state.currentIndex = parseInt(activeItem.getAttribute("data-bs-index"))
+        this.modal.titleElement.querySelector(".carousel-index").innerText = `${this.state.currentIndex + 1} / ${this.state.itemCount}`
     }
 }
 
